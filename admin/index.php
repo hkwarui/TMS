af09aa99771e619a2773fc792815329d796ccfb8
hkwarui <warui.henry@gmail.com>
2021-05-12 11:51:52 +0300

    Add dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,6 @@
 <?php
 include_once('../includes/header.php');
+require_once '../includes/db_config.php';
 
 //DISPLAY A MESSAGE IF ANY 
 if (isset($_SESSION['msg'])) {
@@ -18,11 +19,20 @@ if (isset($_SESSION['msg'])) {
     });
 </script>
 
-<?php 
- ///Dashboard code 
+<?php
+// count courses
+$sth =  $db->prepare('SELECT count(*) FROM courses');
+$sth->execute();
+$totCourses = $sth->fetchColumn();
 
- //course 
- 
+//Count participants 
+$sth =  $db->prepare('SELECT count(*) FROM participants');
+$sth->execute();
+$totParticipants = $sth->fetchColumn();
+
+$sth =  $db->prepare('SELECT count(*) FROM instructor');
+$sth->execute();
+$totInstructor = $sth->fetchColumn();
 ?>
 
 <main class="content">
@@ -34,149 +44,77 @@ if (isset($_SESSION['msg'])) {
             </div>
         </div>
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title mb-4">Courses</h5>
-                        <h1 class="mt-1 mb-3">2.382</h1>
-                        <div class="mb-1">
+                        <h1 class="mt-1 mb-3"><?php echo $totCourses ?></h1>
+                        <!-- <div class="mb-1">
                             <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
                             <span class="text-muted">Since last week</span>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title mb-4">Participants</h5>
-                        <h1 class="mt-1 mb-3">2.382</h1>
-                        <div class="mb-1">
+                        <h1 class="mt-1 mb-3"><?php echo $totParticipants ?></h1>
+                        <!-- <div class="mb-1">
                             <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
                             <span class="text-muted">Since last week</span>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title mb-4">Instrutors</h5>
-                        <h1 class="mt-1 mb-3">2.382</h1>
-                        <div class="mb-1">
+                        <h1 class="mt-1 mb-3"><?php echo $totInstructor ?></h1>
+                        <!-- <div class="mb-1">
                             <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
                             <span class="text-muted">Since last week</span>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title mb-4">Sales</h5>
-                        <h1 class="mt-1 mb-3">2.382</h1>
-                        <div class="mb-1">
-                            <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
-                            <span class="text-muted">Since last week</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             <div class="row">
-                <div class="col-12 col-lg-8 col-xxl-9 d-flex">
-                    <div class="card flex-fill">
-                        <div class="card-header">
+                <div class="card flex-fill">
+                    <div class="card-header">
 
-                            <h5 class="card-title mb-0">Latest Courses</h5>
-                        </div>
-                        <table class="table table-sm table-hover my-0">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th class="d-none d-xl-table-cell">Start Date</th>
-                                    <th class="d-none d-xl-table-cell">End Date</th>
-                                    <th>Status</th>
-                                    <th class="d-none d-md-table-cell">Assignee</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Project Apollo</td>
-                                    <td class="d-none d-xl-table-cell">01/01/2021</td>
-                                    <td class="d-none d-xl-table-cell">31/06/2021</td>
-                                    <td><span class="badge bg-success">Done</span></td>
-                                    <td class="d-none d-md-table-cell">Vanessa Tucker</td>
-                                </tr>
-                                <tr>
-                                    <td>Project Fireball</td>
-                                    <td class="d-none d-xl-table-cell">01/01/2021</td>
-                                    <td class="d-none d-xl-table-cell">31/06/2021</td>
-                                    <td><span class="badge bg-danger">Cancelled</span></td>
-                                    <td class="d-none d-md-table-cell">William Harris</td>
-                                </tr>
-                                <tr>
-                                    <td>Project Hades</td>
-                                    <td class="d-none d-xl-table-cell">01/01/2021</td>
-                                    <td class="d-none d-xl-table-cell">31/06/2021</td>
-                                    <td><span class="badge bg-success">Done</span></td>
-                                    <td class="d-none d-md-table-cell">Sharon Lessman</td>
-                                </tr>
-                                <tr>
-                                    <td>Project Nitro</td>
-                                    <td class="d-none d-xl-table-cell">01/01/2021</td>
-                                    <td class="d-none d-xl-table-cell">31/06/2021</td>
-                                    <td><span class="badge bg-warning">In progress</span></td>
-                                    <td class="d-none d-md-table-cell">Vanessa Tucker</td>
-                                </tr>
-                                <tr>
-                                    <td>Project Phoenix</td>
-                                    <td class="d-none d-xl-table-cell">01/01/2021</td>
-                                    <td class="d-none d-xl-table-cell">31/06/2021</td>
-                                    <td><span class="badge bg-success">Done</span></td>
-                                    <td class="d-none d-md-table-cell">William Harris</td>
-                                </tr>
-                                <tr>
-                                    <td>Project X</td>
-                                    <td class="d-none d-xl-table-cell">01/01/2021</td>
-                                    <td class="d-none d-xl-table-cell">31/06/2021</td>
-                                    <td><span class="badge bg-success">Done</span></td>
-                                    <td class="d-none d-md-table-cell">Sharon Lessman</td>
-                                </tr>
-                                <tr>
-                                    <td>Project Romeo</td>
-                                    <td class="d-none d-xl-table-cell">01/01/2021</td>
-                                    <td class="d-none d-xl-table-cell">31/06/2021</td>
-                                    <td><span class="badge bg-success">Done</span></td>
-                                    <td class="d-none d-md-table-cell">Christina Mason</td>
-                                </tr>
-                                <tr>
-                                    <td>Project Wombat</td>
-                                    <td class="d-none d-xl-table-cell">01/01/2021</td>
-                                    <td class="d-none d-xl-table-cell">31/06/2021</td>
-                                    <td><span class="badge bg-warning">In progress</span></td>
-                                    <td class="d-none d-md-table-cell">William Harris</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <h5 class="card-title mb-0">Latest Courses</h5>
                     </div>
-                </div>
-                <div class="col-12 col-lg-4 col-xxl-3 d-flex">
-                    <div class="card flex-fill w-100">
-                        <div class="card-header">
-
-                            <h5 class="card-title mb-0">Monthly Sales</h5>
-                        </div>
-                        <div class="card-body d-flex w-100">
-                            <div class="align-self-center chart chart-lg">
-                                <canvas id="chartjs-dashboard-bar"></canvas>
-                            </div>
-                        </div>
-                    </div>
+                    <table class="table table-sm table-hover my-0">
+                        <thead>
+                            <tr>
+                                <th> Course Name</th>
+                                <th class="d-none d-md-table-cell">Instructor</th>
+                                <th class="d-none d-xl-table-cell">Duration (Days)</th>
+                                <th class="d-none d-xl-table-cell">Cost</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $stm = $db->prepare("SELECT * FROM courses");
+                            $stm->execute();
+                            while ($row = $stm->fetch()) {
+                            ?>
+                                <tr>
+                                    <td><?php echo $row['courseId'] . " : " . $row['courseName'] ?></td>
+                                    <td class="d-none d-md-table-cell"><?php echo $row['instructor'] ?></td>
+                                    <td class="d-none d-xl-table-cell"><?php echo $row['duration'] ?></td>
+                                    <td><span class="badge bg-success"></span> <?php echo "Ksh. " . $row['cost'] ?></td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-
         </div>
+
+    </div>
 </main>
 
 <footer class="footer">
